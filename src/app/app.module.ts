ae2589c988cf924a38605c208926c960cd244f75
wcc <lisi@163.com>
2017-07-25 11:16:57 +0800

    默认的

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,16 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
 import { AboutPage } from '../pages/about/about';
 import { TypePage } from '../pages/type/type';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ShoppingPage } from '../pages/shopping/shopping';
 import { MycakePage } from '../pages/mycake/mycake';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {ProductPage} from "../pages/type/product/product";
-import {LinkmanPage} from "../pages/mycake/longing/linkman/linkman";
-import {FindPage} from "../pages/mycake/find/find";
-import {AdressPage} from "../pages/mycake/longing/adress/adress";
-import {ChangePage} from "../pages/mycake/longing/change/change";
-import {ChangepassPage} from "../pages/mycake/longing/changepass/changepass";
-import {RegisterPage} from "../pages/mycake/register/register";
-import {PrdetailPage} from "../pages/type/prdetail/prdetail";
-import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
@@ -28,18 +21,10 @@ import {HttpModule} from "@angular/http";
     HomePage,
     TabsPage,
     ShoppingPage,
-    MycakePage,
-    ProductPage,
-    PrdetailPage,
-    LinkmanPage,
-    FindPage,
-    AdressPage,
-    ChangePage,
-    ChangepassPage,
-    RegisterPage
+    MycakePage
   ],
   imports: [
-    BrowserModule,HttpModule,
+    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -50,15 +35,7 @@ import {HttpModule} from "@angular/http";
     HomePage,
     TabsPage,
     ShoppingPage,
-    MycakePage,
-    ProductPage,
-    PrdetailPage,
-    LinkmanPage,
-    FindPage,
-    AdressPage,
-    ChangePage,
-    ChangepassPage,
-    RegisterPage
+    MycakePage
   ],
   providers: [
     StatusBar,
